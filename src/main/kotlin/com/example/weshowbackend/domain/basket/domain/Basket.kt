88f9b4b30f654601f06b276ca6869154e3904531
문정민 <moondeve41@gmail.com>
2023-02-08 17:36:26 +0900

    ADD :: basket domain

---
+++ src/main/kotlin/com/example/weshowbackend/domain/basket/domain/Basket.kt
@@ -0,0 +1,24 @@
+package com.example.weshowbackend.domain.basket.domain
+
+import com.example.weshowbackend.domain.product.domain.Product
+import com.example.weshowbackend.domain.user.domain.User
+import com.example.weshowbackend.global.entity.BaseEntity
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+
+@Entity
+@Table(name = "tbl_basket")
+class Basket (
+
+        @field:NotNull
+        val count : Int,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id", nullable = false)
+        val user: User,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "product_id", nullable = false)
+        val product: Product
+
+) : BaseEntity()
---
+++ src/main/kotlin/com/example/weshowbackend/domain/basket/domain/repository/BasketRepository.kt
@@ -0,0 +1,10 @@
+package com.example.weshowbackend.domain.basket.domain.repository
+
+import com.example.weshowbackend.domain.basket.domain.Basket
+import com.example.weshowbackend.domain.product.domain.Product
+import com.example.weshowbackend.domain.user.domain.User
+import org.springframework.data.repository.CrudRepository
+
+interface BasketRepository : CrudRepository<Basket, Long> {
+    fun findBasketByUserAndProduct(user: User, product: Product): Basket?
+}
