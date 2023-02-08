52740173bb53b5491f5c23ba1c69e4a7d0124747
문정민 <moondeve41@gmail.com>
2023-02-08 17:59:52 +0900

    ADD :: sign logic

---
+++ src/main/kotlin/com/example/weshowbackend/domain/user/service/SignInService.kt
@@ -0,0 +1,33 @@
+package com.example.weshowbackend.domain.user.service
+
+import com.example.weshowbackend.domain.user.domain.repository.UserRepository
+import com.example.weshowbackend.domain.user.exception.BadPasswordException
+import com.example.weshowbackend.domain.user.exception.UserNotFoundException
+import com.example.weshowbackend.domain.user.present.dto.SignInRequest
+import com.example.weshowbackend.domain.user.present.dto.TokenResponse
+import com.example.weshowbackend.global.security.jwt.JwtTokenProvider
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class SignInService (
+        private val userRepository: UserRepository,
+        private val passwordEncoder: PasswordEncoder,
+        private val jwtProvider: JwtTokenProvider
+) {
+
+    @Transactional
+    fun sign(request: SignInRequest): TokenResponse {
+        val user = userRepository.findUserByAccountId(request.accountId) ?: throw UserNotFoundException.EXCEPTION
+
+        if(!passwordEncoder.matches(request.password, user.password)) {
+            throw BadPasswordException.EXCEPTION
+        }
+
+        val access = jwtProvider.getToken(user.accountId)
+        print(access)
+
+        return TokenResponse(access)
+    }
+}
