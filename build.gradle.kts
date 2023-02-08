1cedc13aa6c29ff583f6dd0122da5c544dfa3a51
문정민 <moondeve41@gmail.com>
2023-02-08 17:36:17 +0900

    ADD :: 기본 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,10 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
-}
-
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-repositories {
-    mavenCentral()
+    kotlin("plugin.jpa") version "1.6.10"
+    kotlin("kapt") version "1.6.10"
 }
 
 dependencies {
@@ -21,15 +13,85 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    //jwt
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+    //security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    //validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //lombok
+    implementation("org.projectlombok:lombok:1.18.24")
+
+    //mysql
+    runtimeOnly("mysql:mysql-connector-java")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+allprojects {
+
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
+
+    tasks {
+        compileKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "17"
+            }
+        }
+
+        compileJava {
+            sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+        }
+
+        test {
+            useJUnitPlatform()
+        }
     }
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin.sourceSets.main {
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
