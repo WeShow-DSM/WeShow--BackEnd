ce843cd784476c84f6048d8e0d91d852e7e59135
문정민 <moondeve41@gmail.com>
2023-02-08 18:40:10 +0900

    EDIT :: properties config

--- src/main/kotlin/com/example/weshowbackend/global/config/PropertiesConfig.kt
+++ src/main/kotlin/com/example/weshowbackend/global/config/PropertiesConfig.kt
@@ -3,7 +3,7 @@ package com.example.weshowbackend.global.config
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Configuration
 
-@ConfigurationPropertiesScan(basePackages = ["com.example.demo"])
+@ConfigurationPropertiesScan(basePackages = ["com.example.weshowbackend"])
 @Configuration
 class PropertiesConfig {
 }
