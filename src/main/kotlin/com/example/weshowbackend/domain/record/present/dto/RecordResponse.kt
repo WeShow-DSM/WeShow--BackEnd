e47267bd68f345b285a8bc3b0f35d64511d6f8c1
문정민 <moondeve41@gmail.com>
2023-02-11 03:33:47 +0900

    ADD :: record list service

---
+++ src/main/kotlin/com/example/weshowbackend/domain/record/present/dto/RecordResponse.kt
@@ -0,0 +1,5 @@
+package com.example.weshowbackend.domain.record.present.dto
+
+data class RecordResponse (
+        val records: List<String>
+)
---
+++ src/main/kotlin/com/example/weshowbackend/domain/record/service/RecordListService.kt
@@ -0,0 +1,27 @@
+package com.example.weshowbackend.domain.record.service
+
+import com.example.weshowbackend.domain.record.domain.repository.RecordRepository
+import com.example.weshowbackend.domain.record.present.dto.RecordResponse
+import com.example.weshowbackend.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.stream.Collectors
+
+@Service
+class RecordListService (
+        private val recordRepository: RecordRepository,
+        private val userFacade: UserFacade
+) {
+
+    @Transactional(readOnly = true)
+    fun execute(): RecordResponse {
+        val list = recordRepository.findRecordsByUser(userFacade.getCurrentUser()).stream()
+                .map {
+                    it.content
+                }.collect(Collectors.toList())
+
+        return RecordResponse(
+                records = list.slice(0..10)
+        )
+    }
+}
