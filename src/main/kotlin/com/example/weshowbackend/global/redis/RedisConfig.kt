779d020f1a9e8def4857742aafb92d59db12c890
문정민 <moondeve41@gmail.com>
2023-02-16 20:22:49 +0900

    ADD :: redisconfig

---
+++ src/main/kotlin/com/example/weshowbackend/global/redis/RedisConfig.kt
@@ -0,0 +1,17 @@
+package com.example.weshowbackend.global.redis
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+
+
+@Configuration
+class RedisConfig (
+        private val redisProperties: RedisProperties
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory? {
+        return LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+    }
+}
