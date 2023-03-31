785707c52b55935f4d0f978119ecda26e5002dd9
문정민 <moondeve41@gmail.com>
2023-03-31 17:04:27 +0900

    EDIT :: refreshExp

--- src/main/kotlin/com/example/weshowbackend/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/com/example/weshowbackend/global/security/jwt/JwtProperties.kt
@@ -8,7 +8,6 @@ import org.springframework.boot.context.properties.ConstructorBinding
 class JwtProperties (
         val secretKey: String,
         val accessExp: Long,
-        val refreshExp: Long,
         val header: String,
         val prefix: String
 )
